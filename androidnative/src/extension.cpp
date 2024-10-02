616670aa152abf06ddfdae4441748367c5beee84
Oscar Fernandez <oscar.leif@gmail.com>
2024-10-02 16:52:47 -0500

    Clean up
    Need a custom build for Android.

--- androidnative/src/extension.cpp
+++ androidnative/src/extension.cpp
@@ -6,13 +6,33 @@
 
 // Defold SDK
 #define DLIB_LOG_DOMAIN LIB_NAME
+#include <jni.h>
 #include <dmsdk/sdk.h>
 
 #if defined(DM_PLATFORM_ANDROID)
 
 #include "testlib.h" // Multiply
 
+// #include "glfw/lib/android/android_joystick.h"
+#include <android_native_app_glue.h> //native will always work
+#include <android/input.h>
+#include <android/log.h> //native will always work
+#include <android_native_app_glue.h>
+#include <android/input.h>
+#include <dmsdk/sdk.h>
+#include <stdio.h>
+
+#define LOGI(...) dmLogInfo(__VA_ARGS__)
+#define LOGE(...) dmLogError(__VA_ARGS__)
+#define DEAD_ZONE_THRESHOLD 0.01f
+
+
+//#include "android_joystick.h" //What we need we cannot explose this
+//#include <glfw/glfw_native.h>//Not possible to get
+// #include "external/glfw/lib/android/android_joystick.h" //NO API 
 const char* java_class = "ata.games.defold.android.NativeExample";
+
+extern AInputEvent* g_lastInputEvent;
 
 static JNIEnv* Attach()
 {
@@ -58,6 +78,32 @@ static jclass GetClass(JNIEnv* env, const char* classname)
     env->DeleteLocalRef(str_class_name);
     return outcls;
 }
+
+// Function to expose key press data
+int get_gamepad_key_input(AInputEvent* event) {
+    if (AInputEvent_getType(event) == AINPUT_EVENT_TYPE_KEY && 
+        AInputEvent_getSource(event) & AINPUT_SOURCE_GAMEPAD) {
+        int32_t keycode = AKeyEvent_getKeyCode(event);
+        int32_t action = AKeyEvent_getAction(event);
+        LOGI("Gamepad key event: keycode=%d, action=%d", keycode, action);
+        return keycode;  // Return the keycode
+    }
+    LOGE("Not a gamepad key event.");
+    return -1; // No key input or not a gamepad event
+}
+
+// Function to expose joystick axis values
+float get_joystick_axis_input(AInputEvent* event, int32_t axis) {
+    if (AInputEvent_getType(event) == AINPUT_EVENT_TYPE_MOTION && 
+        AInputEvent_getSource(event) & AINPUT_SOURCE_JOYSTICK) {
+        float axis_value = AMotionEvent_getAxisValue(event, axis, 0);
+        LOGI("Joystick axis input: axis=%d, value=%f", axis, axis_value);
+        return axis_value; // Return the axis value
+    }
+    LOGE("Not a joystick axis event.");
+    return 0.0f;
+}
+
 
 static int DoStuffJava(lua_State* L)
 {
@@ -143,45 +189,105 @@ static int ShowToast(lua_State* L)
     return 0;
 }
 
+// Function to access the latest input event from Lua
+static int Lua_get_latest_input_event(lua_State* L) {
+    AInputEvent* event = g_lastInputEvent;
+
+    if (event == NULL) {
+        // dmLogInfo("No input event available");
+        lua_pushnil(L);
+        return 1;
+    }
+
+    // Example: Get keycode and return it to Lua
+    if (AInputEvent_getType(event) == AINPUT_EVENT_TYPE_KEY) {
+        int32_t keycode = AKeyEvent_getKeyCode(event);
+        lua_pushinteger(L, keycode);
+    } else {
+        lua_pushnil(L);  // Return nil if no key event is available
+    }
+
+    return 1;
+}
+
+static int Lua_get_latest_key_event(lua_State* L) {
+    AInputEvent* event = g_lastInputEvent;
+
+    if (event == NULL) {
+        // dmLogInfo("No input event available");
+        lua_pushnil(L);
+        return 1;
+    }
+
+    int32_t eventType = AInputEvent_getType(event);
+
+    // Check for key events (button presses)
+    if (eventType == AINPUT_EVENT_TYPE_KEY) {
+        int32_t keycode = AKeyEvent_getKeyCode(event);
+        int32_t action = AKeyEvent_getAction(event); // ACTION_DOWN or ACTION_UP
+
+        // Push keycode and action to Lua
+        lua_pushinteger(L, keycode);  // keycode of the pressed key
+        lua_pushinteger(L, action);   // action (pressed or released)
+        return 2;
+    }
+
+    lua_pushnil(L);  // If no key event, return nil
+    return 1;
+}
+
+// Function to access the latest axis event from Lua
+static int Lua_get_latest_axis_event(lua_State* L) {
+    AInputEvent* event = g_lastInputEvent;
+
+    if (event == NULL) {
+        // dmLogInfo("No input event available");
+        lua_pushnil(L);
+        return 1;
+    }
+
+    int32_t eventType = AInputEvent_getType(event);
+    int32_t source = AInputEvent_getSource(event);
+
+    // Check for motion events (axes for joysticks/gamepads)
+    if (eventType == AINPUT_EVENT_TYPE_MOTION) {
+        if (source & AINPUT_SOURCE_JOYSTICK || source & AINPUT_SOURCE_GAMEPAD) {
+            // Extract joystick or gamepad axis values
+            float axis_x = AMotionEvent_getAxisValue(event, AMOTION_EVENT_AXIS_X, 0);
+            float axis_y = AMotionEvent_getAxisValue(event, AMOTION_EVENT_AXIS_Y, 0);
+            float hat_x = AMotionEvent_getAxisValue(event, AMOTION_EVENT_AXIS_HAT_X, 0);
+            float hat_y = AMotionEvent_getAxisValue(event, AMOTION_EVENT_AXIS_HAT_Y, 0);
+
+            // Apply dead zone threshold to filter out near-zero values
+            if (fabs(axis_x) < DEAD_ZONE_THRESHOLD) axis_x = 0.0f;
+            if (fabs(axis_y) < DEAD_ZONE_THRESHOLD) axis_y = 0.0f;
+            if (fabs(hat_x) < DEAD_ZONE_THRESHOLD) hat_x = 0.0f;
+            if (fabs(hat_y) < DEAD_ZONE_THRESHOLD) hat_y = 0.0f;
+
+            // Push axis values to Lua if they are outside the dead zone
+            lua_pushnumber(L, axis_x);  // X-axis value
+            lua_pushnumber(L, axis_y);  // Y-axis value
+            lua_pushnumber(L, hat_x);   // Hat switch X-axis value
+            lua_pushnumber(L, hat_y);   // Hat switch Y-axis value
+            return 4;  // Return 4 values (X, Y, HAT_X, HAT_Y)
+        }
+    }
+
+    lua_pushnil(L);  // If no axis event, return nil
+    return 1;
+}
+
 static int InitializeNativeExample(lua_State* L) {
     AttachScope attachscope;
     JNIEnv* env = attachscope.m_Env;
 
     jobject activity = dmGraphics::GetNativeAndroidActivity();    
-    // jclass native_example_class = env->FindClass("ata/games/defold/android/NativeExample");
     jclass native_example_class = GetClass(env, java_class);
 
     jmethodID set_activity = env->GetStaticMethodID(native_example_class, "setMainActivity", "(Landroid/app/Activity;)V");
     env->CallStaticVoidMethod(native_example_class, set_activity, activity);
 
     return 0;
-}
-
-static int Lua_IsButtonPressed(lua_State* L)
-{    
-    AttachScope attachscope;
-    int keyCode = luaL_checkinteger(L,1);
-    JNIEnv* env = attachscope.m_Env;
-    jclass native_example_class = GetClass(env, java_class);
-    jmethodID is_button_pressed = env->GetStaticMethodID(native_example_class, "isButtonPressed", "(I)Z");
-    jboolean result = env->CallStaticBooleanMethod(native_example_class, is_button_pressed, keyCode);
-    lua_pushboolean(L, result);
-    return 1;
-}
-
-static int Lua_GetAxisX(lua_State* L)
-{
-    AttachScope attachscope;
-    JNIEnv* env = attachscope.m_Env;
-    // Get the class reference for NativeExample
-    jclass native_example_class = GetClass(env, java_class);    
-    // Get the method ID for getAxisX
-    jmethodID get_axis_x = env->GetStaticMethodID(native_example_class, "getAxisX", "()F");    
-    // Call the static method to get the axis X value
-    jfloat axisX = env->CallStaticFloatMethod(native_example_class, get_axis_x);    
-    // Push the float value onto the Lua stack
-    lua_pushnumber(L, axisX);    
-    return 1; // Return the number of results
 }
 
 static int GetRaw(lua_State* L)
@@ -263,24 +369,6 @@ static int CheckSystemFeature(lua_State* L)
     return 1;
 }
 
-static int LuaPollGamepadAxes(lua_State* L)
-{
-    DM_LUA_STACK_CHECK(L, 0);
-    AttachScope attachscope;
-    JNIEnv* env = attachscope.m_Env;
-
-    jclass cls = GetClass(env, java_class);
-    jmethodID poll_method = env->GetStaticMethodID(cls, "pollGamepadAxes", "()V");
-    env->CallStaticVoidMethod(cls, poll_method);
-    return 0;
-}
-
-static int LuaGetAxisX(lua_State* L)
-{
-
-    return 1;
-}
-
 // Functions exposed to Lua
 static const luaL_reg Module_methods[] =
 {
@@ -294,9 +382,9 @@ static const luaL_reg Module_methods[] =
     {"is_playing_on_tv", IsPlayingOnTV}, 
     {"check_system_feature", CheckSystemFeature},
     {"initialize", InitializeNativeExample},
-    {"is_button_pressed", Lua_IsButtonPressed},
-    {"pool_gamepad_axes", LuaPollGamepadAxes}, //Poll must be called first
-    {"get_axis_x", Lua_GetAxisX}, 
+    {"get_latest_input_event", Lua_get_latest_input_event},
+    {"get_latest_key_event", Lua_get_latest_key_event},
+    {"get_latest_axis_event", Lua_get_latest_axis_event},
     {0, 0}
 };
 
